3d3aa1d623adbcf6d8ff7714a1d77e64e94232ae
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-05-08 11:43:28 +0100

    generated the spring-bootproject for the API

---
+++ back_end/SchemaShovelWebAPI/src/main/java/uk/mddeveloper/SchemaShovelWebAPI/SchemaShovelWebApiApplication.java
@@ -0,0 +1,13 @@
+package uk.mddeveloper.SchemaShovelWebAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaShovelWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaShovelWebApiApplication.class, args);
+	}
+
+}
---
+++ back_end/SchemaShovelWebAPI/src/test/java/uk/mddeveloper/SchemaShovelWebAPI/SchemaShovelWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.mddeveloper.SchemaShovelWebAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaShovelWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
