e16d5cd004a9dee168350a2df43d21b59df67405
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-05-10 11:28:49 +0100

    setup the DatabaseLoadOptions component and added it to DatabaseSelection

---
+++ front_end/src/components/DatabaseLoadOptions/DatabaseLoadOptions.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './DatabaseLoadOptions.css';
+import PropTypes from 'prop-types';
+
+class DatabaseLoadOptions extends Component
+{
+	
+	
+	render()
+	{
+		return (
+			<div className="DatabaseLoadOptions">
+				<button 
+					// We check if it's NAN specifically, as 0 is falsy
+					disabled={this.props.selectedDatabaseIndex < 0} 
+					onClick={() => this.props.loadSelectedDatabase(this.props.selectedDatabaseIndex)}>
+				Load Database</button>
+				
+				{/* This will eventually take you to the database upload route */}
+				<button onClick={() => {}}>Add New Database</button>
+			</div>
+		);
+	}
+}
+
+DatabaseLoadOptions.propTypes = {
+	loadSelectedDatabase: PropTypes.func.isRequired,
+	selectedDatabaseIndex: PropTypes.number.isRequired,
+};
+
+export default DatabaseLoadOptions;
--- front_end/src/components/DatabaseSelection/DatabaseSelection.js
+++ front_end/src/components/DatabaseSelection/DatabaseSelection.js
@@ -3,6 +3,7 @@ import './DatabaseSelection.css'
 import PropTypes from 'prop-types';
 import SelectableList from '../SelectableList/SelectableList';
 import EditableItem from '../EditableItem/EditableItem';
+import DatabaseLoadOptions from '../DatabaseLoadOptions/DatabaseLoadOptions';
 
 class DatabaseSelection extends Component
 {	
@@ -19,13 +20,15 @@ class DatabaseSelection extends Component
 					<EditableItem text="Test 2" saveChanges={() => {}} />
 					<EditableItem text="Test 3" saveChanges={() => {}} />
 				</SelectableList>
+				<DatabaseLoadOptions loadSelectedDatabase={() => {}} 
+						selectedDatabaseIndex={this.props.selectedDatabaseIndex} />
 			</div>
 		);
 	}
 }
 
 DatabaseSelection.propTypes = {
-	selectedDatabaseIndex: PropTypes.number,
+	selectedDatabaseIndex: PropTypes.number.isRequired,
 	setSelectedDatabaseIndex: PropTypes.func.isRequired
 };
 
