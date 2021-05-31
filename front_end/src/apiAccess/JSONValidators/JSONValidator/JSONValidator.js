899bd25b1152f36dff896c2fd95f908d4b87d305
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-05-31 11:37:42 +0100

    moved some of the single item validation methods from DatabaseJSONValidator to JSONValidator

--- front_end/src/apiAccess/JSONValidators/JSONValidator/JSONValidator.js
+++ front_end/src/apiAccess/JSONValidators/JSONValidator/JSONValidator.js
@@ -20,12 +20,38 @@ export default class JSONValidator
 		return this._errorQueue.dequeue();
 	}
 	
-	validateJSON()
+	validateJSON(json)
 	{
 		return true;
 	}
 	
 	
+	
+	//item could be a schema/table/column
+	//item index is the index of the item in it's respective array
+	//itemName could be "schema"/"table"/"column"
+	_validateSingleItem(item, itemIndex, itemName, validProperties, addError)
+	{
+		this._checkForInvalidProperties(validProperties, item, itemIndex, itemName, addError);
+		
+		validProperties.forEach((prop) => {
+			const optional = (prop.hasOwnProperty("optional") && prop.optional);
+			this._validateProperty(item, itemIndex, itemName, prop.name, prop.type, addError, optional);
+		});
+	}
+	
+	
+	_checkForInvalidProperties(validProperties, item, itemIndex, itemName, addError)
+	{
+		const SentenceStartItemName = this._getWordWithCapitalisedFirstLetter(itemName);
+		
+		//Using for..in would include prototype entries, so not using that
+		for(const [key, value] of Object.entries(item))
+		{
+			if(validProperties.filter(vp => vp.name === key).length === 0)
+				addError(`${SentenceStartItemName} at index ${itemIndex} contains an invalid property: ${key}.`);
+		}
+	}
 	
 	_propertyIsOfType(item, propName, propType)
 	{
@@ -37,5 +63,27 @@ export default class JSONValidator
 		{
 			return (typeof item[propName] === propType);
 		}
+	}
+	
+	//propType for arrays is just "array"
+	_validateProperty(item, itemIndex, itemName, propName, propType, addError, optional = false)
+	{
+		const SentenceStartItemName = this._getWordWithCapitalisedFirstLetter(itemName);
+		
+		if(item.hasOwnProperty(propName))
+		{
+			if(!this._propertyIsOfType(item, propName, propType))
+				addError(`${SentenceStartItemName}'s ${propName} property at index ${itemIndex} is not a valid ${propType}.`);
+		}
+		else if(!optional)
+		{
+			addError(`${SentenceStartItemName} at index ${itemIndex} does not have a ${propName} property.`);
+		}
+	}
+	
+	
+	_getWordWithCapitalisedFirstLetter(word)
+	{
+		return word.charAt(0).toUpperCase() + word.slice(1);;
 	}
 }
