1850738375cf62443c7d57db40b5e65ee7790d9c
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-06-05 14:22:19 +0100

    the DatabaseController method for updating the DB name was a PUT, not PATCH. Corrected this

--- back_end/SchemaShovelWebAPI/src/main/java/uk/mddeveloper/SchemaShovelWebAPI/Controllers/DatabaseController.java
+++ back_end/SchemaShovelWebAPI/src/main/java/uk/mddeveloper/SchemaShovelWebAPI/Controllers/DatabaseController.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -145,7 +145,7 @@ public class DatabaseController {
 	}
 	
 	
-	@PutMapping("/{id}")
+	@PatchMapping("/{id}")
 	Database update(@RequestBody Database newDatabase, @PathVariable Long id) 
 			throws RecordNotFoundException, UnprocessableEntityException, InternalServerErrorException
 	{
