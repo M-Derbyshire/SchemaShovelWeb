8ad5d0c9101ff032d2acd647385d0f6d7fec31dc
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-06-08 20:49:26 +0100

    delegated the APIAccessor database entity methods into the DatabaseEntityAccessor class

--- front_end/src/apiAccess/APIAccessor/APIAccessor.js
+++ front_end/src/apiAccess/APIAccessor/APIAccessor.js
@@ -1,6 +1,5 @@
 import Queue from '../Queue/Queue';
-import JSONArrayValidator from '../JSONValidators/JSONArrayValidator/JSONArrayValidator';
-import DatabaseJSONValidator from '../JSONValidators/DatabaseJSONValidator/DatabaseJSONValidator';
+import DatabaseEntityAccessor from './EntityAccessors/DatabaseEntityAccessor';
 
 export default class APIAccessor
 {
@@ -8,16 +7,14 @@ export default class APIAccessor
 	{
 		this._baseURL = baseURL;
 		
-		const validProperties = [
-			{ name: "id", type: "number" },
-			{ name: "name", type: "string" }
-		];		
-		this._dbIDAndNameJSONValidator = new JSONArrayValidator("database", validProperties);
-		this._userDBJSONValidator = new DatabaseJSONValidator(false);
-		this._apiDBJSONValidator = new DatabaseJSONValidator(true);
-		
 		this._errorQueue = new Queue();
 		this._addError = this._errorQueue.enqueue.bind(this._errorQueue);
+		
+		this._databaseEntityAccessor = new DatabaseEntityAccessor(
+			this._baseURL, 
+			this._getJSONFromAPI, 
+			this._addError
+		);
 	}
 	
 	
@@ -54,169 +51,26 @@ export default class APIAccessor
 	
 	async getDatabaseList()
 	{
-		try
-		{
-			const [dbListText, dbList] = await this._getJSONFromAPI(this._baseURL + "/databases/");
-			
-			this._validateIDAndNameJSON(dbListText);
-			
-			return dbList;
-		}
-		catch(err)
-		{
-			this._addError(`Error while loading database list: ${err.message}`);
-			return [];
-		}
+		return await this._databaseEntityAccessor.getDatabaseList();
 	}
-	
-	
 	
 	async getDatabaseByID(id)
 	{
-		try
-		{
-			const [dbText, db] = await this._getJSONFromAPI(this._baseURL + "/databases/" + id);
-			
-			this._validateDatabaseJSON(`${dbText}`, false);
-			
-			return db;
-		}
-		catch(err)
-		{
-			this._addError(`Error while loading database record: ${err.message}`);
-			return {};
-		}
+		return await this._databaseEntityAccessor.getDatabaseByID(id);
 	}
-	
-	
-	
 	
 	async createDatabase(newDB)
 	{
-		let dbText, db;
-		
-		try
-		{
-			this._validateDatabaseJSON(newDB, true);
-		}
-		catch(preCreationError)
-		{
-			this._addError(`Error while creating database record: ${preCreationError.message}`);
-			return {};
-		}
-		
-		try
-		{
-			const settings = {
-				method: "POST",
-				headers: {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json'
-				},
-				body: newDB
-			};
-			
-			[dbText, db] = await this._getJSONFromAPI(this._baseURL + "/databases", settings);
-		}
-		catch(duringCreationError)
-		{
-			this._addError(`Error during creation of database record: ${duringCreationError.message}`);
-			return {};
-		}
-		
-		try
-		{
-			this._validateDatabaseJSON(dbText, false);
-		}
-		catch(postCreationError)
-		{
-			this._addError(`Error after creation of database record: ${postCreationError.message}`);
-			return {};
-		}
-		
-		return db;
+		return await this._databaseEntityAccessor.createDatabase(newDB);
 	}
-	
-	
-	
 	
 	async updateDatabaseName(id, newName)
 	{
-		try
-		{
-			if(typeof newName !== "string") throw new Error("The provided name was not a valid string");
-			
-			const settings = {
-				method: "PATCH",
-				headers: {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({
-					name: newName
-				})
-			};
-			
-			const [dbText, db] = await this._getJSONFromAPI(this._baseURL + "/databases/" + id, settings);
-			
-			this._validateDatabaseJSON(`${dbText}`, false);
-			
-			return db;
-		}
-		catch(err)
-		{
-			this._addError(`Error while updating database record: ${err.message}`);
-			return {};
-		}
+		return await this._databaseEntityAccessor.updateDatabaseName(id, newName);
 	}
-	
-	
 	
 	async deleteDatabase(id)
 	{
-		try
-		{
-			const response = await fetch(`${this._baseURL}/databases/${id}`, {
-				method: "DELETE"
-			});
-		
-			if(response.ok)
-			{
-				return true
-			}
-			else
-			{
-				throw new Error(response.statusText);
-			}
-		}
-		catch(err)
-		{
-			this._addError(`Error while deleting database record: ${err.message}.`);
-			return false;
-		}
-	}
-	
-	
-	
-	//Throws error
-	_validateIDAndNameJSON(jsonText)
-	{
-		this._validateJSONWithValidator(jsonText, this._dbIDAndNameJSONValidator);
-	}
-	
-	//Throws error
-	_validateDatabaseJSON(jsonText, jsonIsFromUser)
-	{
-		const validator = (jsonIsFromUser) ? this._userDBJSONValidator : this._apiDBJSONValidator;
-		this._validateJSONWithValidator(jsonText, validator);
-	}
-	
-	//Throws error
-	_validateJSONWithValidator(jsonText, validator)
-	{
-		if(!validator.validateJSON(`${jsonText}`))
-		{
-			throw new Error(validator.getNextError());
-		}
+		return await this._databaseEntityAccessor.deleteDatabase(id);
 	}
 }
