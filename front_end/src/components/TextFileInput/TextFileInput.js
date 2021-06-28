7742a9f721d66031022c0973510f73e24c034592
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-06-28 20:57:48 +0100

    Setup the TextFileInput component (still need to write tests)

---
+++ front_end/src/components/TextFileInput/TextFileInput.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './TextFileInput.css';
+import PropTypes from 'prop-types';
+
+class TextFileInput extends Component
+{	
+	textAreaOnChange(e)
+	{
+		this.props.setFileText(e.target.value);
+	}
+	
+	fileInputOnChange(e)
+	{
+		const fileReader = new FileReader();
+		fileReader.onload = (fileReadEvent) => {
+			try
+			{
+				this.props.setFileText(fileReadEvent.target.result);
+			}
+			catch(err)
+			{
+				this.props.onErrorHandler(err.message);
+			}
+		};
+		
+		fileReader.readAsText(e.target.files[0]);
+	}
+	
+	render()
+	{
+		const textAreaValue = (!!this.props.fileText) ? this.props.fileText : "";
+		
+		return (
+			<div className="TextFileInput">
+				<textarea 
+					className="TextFileTextArea"
+					placeholder="Schema JSON" 
+					onChange={this.textAreaOnChange.bind(this)} 
+					value={textAreaValue}>
+				</textarea>
+				
+				<input
+					type="file"
+					className="TextFileFileInput"
+					onChange={this.fileInputOnChange.bind(this)} />
+			</div>
+		);
+	}
+}
+
+TextFileInput.propTypes = {
+	fileText: PropTypes.string,
+	setFileText: PropTypes.func.isRequired,
+	onErrorHandler: PropTypes.func.isRequired //Should be a function that takes the error text
+};
+
+export default TextFileInput;
