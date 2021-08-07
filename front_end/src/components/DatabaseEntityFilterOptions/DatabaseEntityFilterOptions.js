db4d95cf974b7d7daabc28db870b7b392aa7635c
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-08-07 14:25:21 +0100

    wrote, styled and tested DatabaseEntityFilterOptions

---
+++ front_end/src/components/DatabaseEntityFilterOptions/DatabaseEntityFilterOptions.js
@@ -0,0 +1,180 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './DatabaseEntityFilterOptions.css';
+
+class DatabaseEntityFilterOptions extends Component
+{
+	constructor(props)
+	{
+		super(props);
+		
+		this.state = {
+			filterSchemaText: "",
+			filterTableText: "",
+			filterColumnText: "",
+			includeDescriptionText: false,
+			fkSelectedSchemaID: -1,
+			fkSelectedTableID: -1
+		};
+	}
+	
+	
+	runTextFilterHandler()
+	{
+		this.props.runTextFilter(
+			this.state.filterSchemaText, 
+			this.state.filterTableText, 
+			this.state.filterColumnText,
+			this.state.includeDescriptionText
+		);
+	}
+	
+	clearTextFilter()
+	{
+		this.setState({
+			filterSchemaText: "",
+			filterTableText: "",
+			filterColumnText: ""
+		});
+		
+		this.props.runTextFilter("", "", "", this.state.includeDescriptionText);
+	}
+	
+	runFKFilterHandler()
+	{
+		this.props.runFkFilter(this.state.fkSelectedTableID);
+	}
+	
+	clearFKFilter()
+	{
+		this.setState({
+			fkSelectedSchemaID: -1,
+			fkSelectedTableID: -1
+		});
+		
+		this.props.runFkFilter(-1);
+	}
+	
+	
+	schemaSelectOptionsMapper()
+	{
+		return this.props
+			.schemas.map((schema, i) => (<option value={schema.id} key={i}>{schema.name}</option>));
+	}
+	
+	tableSelectOptionsMapper()
+	{
+		const schemaID = this.state.fkSelectedSchemaID;
+		
+		if(schemaID < 0)
+			return;
+		
+		const tableList = this.props.schemas.filter(s => s.id === schemaID)[0].childEntities;
+		
+		return tableList.map((table, i) => (<option value={table.id} key={i}>{table.name}</option>));
+	}
+	
+	render()
+	{
+		// console.log(this.state.fkSelectedSchemaID);
+		
+		return (
+			<div className="DatabaseEntityFilterOptions">
+				
+				<hr/>
+				
+				<div className="FilterOptionsRow textFilterOptions">
+					
+					<div className="FilterOptionsControl">
+						<label>Schema Filter: </label>
+						<input type="text" className="textFilterInput"
+							value={this.state.filterSchemaText} 
+							onChange={(e) => this.setState({ filterSchemaText: e.target.value })} />
+					</div>
+					
+					<div className="FilterOptionsControl">
+						<label>Table Filter: </label>
+						<input type="text" className="textFilterInput"
+							value={this.state.filterTableText} 
+							onChange={(e) => this.setState({ filterTableText: e.target.value })} />
+					</div>
+					
+					<div className="FilterOptionsControl">
+						<label>Column Filter: </label>
+						<input type="text" className="textFilterInput"
+							value={this.state.filterColumnText} 
+							onChange={(e) => this.setState({ filterColumnText: e.target.value })} />
+					</div>
+					
+					<div className="FilterOptionsControl">
+						<label>Include description text in filter? </label>
+						<input type="checkBox" className="descFilterCheckbox"
+							checked={this.state.includeDescriptionText} 
+							onChange={(e) => this.setState({ includeDescriptionText: e.target.checked })} />
+					</div>
+					
+					<div className="FilterOptionsControl">
+						<button onClick={this.runTextFilterHandler.bind(this)} className="textFilterRunBtn">
+							Filter
+						</button>
+						<button onClick={this.clearTextFilter.bind(this)} className="textFilterClearBtn">
+							Clear
+						</button>
+					</div>
+				</div>
+				
+				<hr/>
+				
+				<div className="FilterOptionsRow fkFilterOptions">
+					<h2>Find Foreign Keys of: </h2>
+					
+					<div className="FilterOptionsControl">
+						<label>Schema: </label>
+						<select value={this.state.fkSelectedSchemaID} onChange={(e) => this.setState({
+							fkSelectedSchemaID: Number(e.target.value),
+							fkSelectedTableID: -1
+						})} className="fkEntitySelect fkSchemaSelect">
+							<option value="-1"></option>
+							{this.schemaSelectOptionsMapper.apply(this)}
+						</select>
+					</div>
+					
+					<div className="FilterOptionsControl">
+						<label>Table: </label>
+						<select value={this.state.fkSelectedTableID} onChange={(e) => this.setState({
+							fkSelectedTableID: Number(e.target.value)
+						})} className="fkEntitySelect fkTableSelect">
+							<option value="-1"></option>
+							{this.tableSelectOptionsMapper.apply(this)}
+						</select>
+					</div>
+					
+					<div className="FilterOptionsControl">
+						<button 
+							onClick={this.runFKFilterHandler.bind(this)}
+							disabled={this.state.fkSelectedTableID < 0} 
+							className="fkFilterRunBtn" >Find</button>
+							
+						<button onClick={this.clearFKFilter.bind(this)}
+							className="fkFilterClearBtn">Clear</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+DatabaseEntityFilterOptions.propTypes = {
+	
+	//Required, but can be an empty list if not yet retrieved.
+	//Should be an array of schemas, from a FilterableSchemaList method
+	schemas: PropTypes.array.isRequired,
+	
+	//Should take 4 params: schemaFilterText, tableFilterText, columnFilterText, includeDescriptionText
+	runTextFilter: PropTypes.func.isRequired,
+	
+	//Should take the target table ID as a parameter
+	runFkFilter: PropTypes.func.isRequired
+};
+
+export default DatabaseEntityFilterOptions;
---
+++ front_end/src/components/DatabaseEntityFilterOptions/DatabaseEntityFilterOptions.test.js
@@ -0,0 +1,191 @@
+import ReactTestUtils from 'react-dom/test-utils';
+import DatabaseEntityFilterOptions from './DatabaseEntityFilterOptions';
+
+const dummyFuncProp = () => {};
+const bareTestSchemas = [
+	{ id: 1, name: "aSchema", childEntities: [
+		{ id: 1, name: "aTable", childEntities: [
+			{ id: 1, name: "aColumn" }
+		]}
+	]},
+	{ id: 2, name: "aSchema", childEntities: [
+		{ id: 2, name: "aTable", childEntities: [
+			{ id: 2, name: "aColumn" }
+		]}
+	]}
+];
+
+test("using the text-filter clear button will clear the filters (but not the description checkbox)", () => {
+	
+	const options = ReactTestUtils.renderIntoDocument(
+		<DatabaseEntityFilterOptions schemas={[]} runTextFilter={dummyFuncProp} runFkFilter={dummyFuncProp} />
+	);
+	
+	const filterInputs = ReactTestUtils.scryRenderedDOMComponentsWithClass(options, "textFilterInput");
+	const descCheckbox = ReactTestUtils.findRenderedDOMComponentWithClass(options, "descFilterCheckbox");
+	const clearFilterBtn = ReactTestUtils.findRenderedDOMComponentWithClass(options, "textFilterClearBtn");
+	
+	filterInputs.forEach(fi => ReactTestUtils.Simulate.change(fi, { target: { value: 'testing' } }));
+	ReactTestUtils.Simulate.change(descCheckbox, { target: { checked: true } });
+	ReactTestUtils.Simulate.click(clearFilterBtn);
+	
+	filterInputs.forEach(fi => expect(fi.value).toEqual(""));
+	expect(descCheckbox.checked).toBeTruthy();
+});
+
+
+test.each([
+	[true],
+	[false]
+])("the text-filter clear button will call the runTextFilter prop with empty strings, but with the right description checkbox value", (checkCheckbox) => {
+	
+	const mockRunTextFilter = jest.fn();
+	
+	const options = ReactTestUtils.renderIntoDocument(
+		<DatabaseEntityFilterOptions 
+			schemas={[]} runTextFilter={mockRunTextFilter} runFkFilter={dummyFuncProp} />
+	);
+	const descCheckbox = ReactTestUtils.findRenderedDOMComponentWithClass(options, "descFilterCheckbox");
+	const clearFilterBtn = ReactTestUtils.findRenderedDOMComponentWithClass(options, "textFilterClearBtn");
+	
+	ReactTestUtils.Simulate.change(descCheckbox, { target: { checked: checkCheckbox } });
+	ReactTestUtils.Simulate.click(clearFilterBtn);
+	
+	expect(mockRunTextFilter).toHaveBeenCalledWith("", "", "", checkCheckbox);
+	
+});
+
+
+test.each([
+	[["schemaTest1", "tableTest1", "columnTest1"], true],
+	[["schemaTest2", "tableTest2", "columnTest2"], false]
+])("the text-filter filter button will call the runTextFilter prop function, with the given values", 
+	(filterTextValues, checkCheckbox) => {
+	
+	const mockRunTextFilter = jest.fn();
+	
+	const options = ReactTestUtils.renderIntoDocument(
+		<DatabaseEntityFilterOptions 
+			schemas={[]} runTextFilter={mockRunTextFilter} runFkFilter={dummyFuncProp} />
+	);
+	
+	const filterInputs = ReactTestUtils.scryRenderedDOMComponentsWithClass(options, "textFilterInput");
+	const descCheckbox = ReactTestUtils.findRenderedDOMComponentWithClass(options, "descFilterCheckbox");
+	const runFilterBtn = ReactTestUtils.findRenderedDOMComponentWithClass(options, "textFilterRunBtn");
+	
+	expect(filterInputs.length).toEqual(filterTextValues.length);
+	filterInputs.forEach((fi, index) => {
+		ReactTestUtils.Simulate.change(fi, { target: { value: filterTextValues[index] } });
+	});
+	
+	ReactTestUtils.Simulate.change(descCheckbox, { target: { checked: checkCheckbox } });
+	ReactTestUtils.Simulate.click(runFilterBtn);
+	
+	expect(mockRunTextFilter).toHaveBeenCalledWith.apply(this, [...filterTextValues, checkCheckbox]);
+	
+});
+
+
+test("using the fk-filter clear button will clear the lists back to -1", () => {
+	
+	const options = ReactTestUtils.renderIntoDocument(
+		<DatabaseEntityFilterOptions 
+			schemas={bareTestSchemas} runTextFilter={dummyFuncProp} runFkFilter={dummyFuncProp} />
+	);
+	
+	const schemaSelect = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkSchemaSelect");
+	const tableSelect = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkTableSelect");
+	const clearBtn = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkFilterClearBtn");
+	
+	ReactTestUtils.Simulate.change(schemaSelect, { target: { value: "1" } });
+	ReactTestUtils.Simulate.change(tableSelect, { target: { value: "1" } });
+	ReactTestUtils.Simulate.click(clearBtn);
+	
+	expect(schemaSelect.value).toEqual("-1");
+	expect(tableSelect.value).toEqual("-1");
+});
+
+
+test("the fk-filter clear button will call the runFKFilter prop with -1 as the passed id", () => {
+	
+	const mockRunFkFilter = jest.fn();
+	
+	const options = ReactTestUtils.renderIntoDocument(
+		<DatabaseEntityFilterOptions 
+			schemas={bareTestSchemas} runTextFilter={dummyFuncProp} runFkFilter={mockRunFkFilter} />
+	);
+	
+	const schemaSelect = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkSchemaSelect");
+	const tableSelect = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkTableSelect");
+	const clearBtn = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkFilterClearBtn");
+	
+	ReactTestUtils.Simulate.change(schemaSelect, { target: { value: "1" } });
+	ReactTestUtils.Simulate.change(tableSelect, { target: { value: "1" } });
+	ReactTestUtils.Simulate.click(clearBtn);
+	
+	expect(mockRunFkFilter).toHaveBeenCalledWith(-1);
+});
+
+// looking at bareTestSchemas at the top of this file, each tableID is the same as it's schemaID, 
+//so these represent both
+test.each([
+	[1],
+	[2]
+])("the fk-filter filter button will call the runFKFilter prop function, with the selected table ID", (schemaAndTableID) => {
+	
+	const mockRunFkFilter = jest.fn();
+	
+	const options = ReactTestUtils.renderIntoDocument(
+		<DatabaseEntityFilterOptions 
+			schemas={bareTestSchemas} runTextFilter={dummyFuncProp} runFkFilter={mockRunFkFilter} />
+	);
+	
+	const schemaSelect = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkSchemaSelect");
+	const tableSelect = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkTableSelect");
+	const runBtn = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkFilterRunBtn");
+	
+	// looking at bareTestSchemas at the top of this file, each tableID is the same as it's schemaID, 
+	//so using the same variable
+	ReactTestUtils.Simulate.change(schemaSelect, { target: { value: schemaAndTableID } });
+	ReactTestUtils.Simulate.change(tableSelect, { target: { value: schemaAndTableID } });
+	ReactTestUtils.Simulate.click(runBtn);
+	
+	expect(mockRunFkFilter).toHaveBeenCalledWith(schemaAndTableID);
+	
+});
+
+
+test("the initial values of the fk-filter schema and table selects will be -1", () => {
+	
+	const options = ReactTestUtils.renderIntoDocument(
+		<DatabaseEntityFilterOptions 
+			schemas={bareTestSchemas} runTextFilter={dummyFuncProp} runFkFilter={dummyFuncProp} />
+	);
+	
+	const schemaSelect = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkSchemaSelect");
+	const tableSelect = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkTableSelect");
+	
+	expect(schemaSelect.value).toEqual("-1");
+	expect(tableSelect.value).toEqual("-1");
+});
+
+
+test("if there is no table selected for the fk-filter, the fk filter button will be disabled", () => {
+	
+	const options = ReactTestUtils.renderIntoDocument(
+		<DatabaseEntityFilterOptions 
+			schemas={bareTestSchemas} runTextFilter={dummyFuncProp} runFkFilter={dummyFuncProp} />
+	);
+	
+	const schemaSelect = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkSchemaSelect");
+	const tableSelect = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkTableSelect");
+	const runBtn = ReactTestUtils.findRenderedDOMComponentWithClass(options, "fkFilterRunBtn");
+	
+	expect(runBtn).toBeDisabled();
+	
+	ReactTestUtils.Simulate.change(schemaSelect, { target: { value: 1 } });
+	expect(runBtn).toBeDisabled();
+	
+	ReactTestUtils.Simulate.change(tableSelect, { target: { value: 1 } });
+	expect(runBtn).not.toBeDisabled();
+});
