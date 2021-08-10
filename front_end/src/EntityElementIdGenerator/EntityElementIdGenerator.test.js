de6adf554c2b4e066b871d70e57eb705e49c58e2
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-08-10 21:11:28 +0100

    wrote an extra test for EntityElementIdGenerator, to make sure the values from the different methods are unique from each other

--- front_end/src/EntityElementIdGenerator/EntityElementIdGenerator.test.js
+++ front_end/src/EntityElementIdGenerator/EntityElementIdGenerator.test.js
@@ -41,3 +41,20 @@ test.each([
 	expect(result).toEqual(expect.stringContaining(id.toString()));
 	expect(result).toEqual(expect.stringContaining(name));
 });
+
+test.each([
+	[1, "test1"],
+	[2, "test2"],
+	[3, "test3"]
+])("all 3 get-ID methods will return unique values, when given the same id and name", (id, name) => {
+	
+	const generator = new EntityElementIdGenerator();
+	
+	const schemaResult = generator.getIdForSchema(id, name);
+	const tableResult = generator.getIdForTable(id, name);
+	const columnResult = generator.getIdForColumn(id, name);
+	
+	expect(schemaResult).not.toEqual(tableResult);
+	expect(tableResult).not.toEqual(columnResult);
+	expect(columnResult).not.toEqual(schemaResult);
+});
