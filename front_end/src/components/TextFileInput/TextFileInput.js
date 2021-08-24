31235f2b613cf5fec09ff002839a452f190d4017
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-08-24 18:07:02 +0100

    Wrote the documentation comments for the TextFileInput

--- front_end/src/components/TextFileInput/TextFileInput.js
+++ front_end/src/components/TextFileInput/TextFileInput.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import './TextFileInput.css';
 import PropTypes from 'prop-types';
 
+/**
+* Provides a textarea element, that can also be populated with text from a file, via a file input element
+*
+*@component
+ */
 class TextFileInput extends Component
 {	
+	/**
+	* Runs when the textarea value is changed, and passes the value on to this.props.setFileText()
+	* @param {object} e - The standard onChange event parameter
+	 */
 	textAreaOnChange(e)
 	{
 		this.props.setFileText(e.target.value);
 	}
 	
+	/**
+	* Handles the file input element onChange event. Gets the data from the file (or passes an error to 
+	* this.props.onErrorHandler) and puts that text data into the textarea element
+	* @param {object} e - The standard onChange event parameter
+	 */
 	fileInputOnChange(e)
 	{
 		const fileReader = new FileReader();
@@ -24,6 +38,7 @@ class TextFileInput extends Component
 		}
 	}
 	
+	/** Render a TextFileInput */
 	render()
 	{
 		const textAreaValue = (this.props.fileText) ? this.props.fileText : "";
@@ -50,10 +65,31 @@ class TextFileInput extends Component
 }
 
 TextFileInput.propTypes = {
+	/**
+	* The text for the textarea element
+	 */
 	fileText: PropTypes.string,
+	
+	/**
+	* A function that sets the text for the textarea element
+	* @param {string} text - The new text value
+	 */
 	setFileText: PropTypes.func.isRequired,
-	onErrorHandler: PropTypes.func.isRequired, //Should be a function that takes the error text
+	
+	/**
+	* A function used to handle errors.
+	* @param {string} error - The error message
+	 */
+	onErrorHandler: PropTypes.func.isRequired,
+	
+	/**
+	* Is this input disabled?
+	 */
 	disabled: PropTypes.bool,
+	
+	/**
+	* Is this input's value required, or can it be left empty?
+	 */
 	required: PropTypes.bool
 };
 
